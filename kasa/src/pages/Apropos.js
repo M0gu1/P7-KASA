5276726f9681693582194aae0694f08b1b17f5b7
Morgane Fisichella <morgane.fisichella@outlook.fr>
2023-05-05 12:35:52 +0200

    commit_5_mai

--- kasa/src/pages/Apropos.js
+++ kasa/src/pages/Apropos.js
@@ -1,42 +1,51 @@
 import React from 'react'
+import { useState } from 'react'
 import Header from '../components/Header'
 
 const Apropos = () => {
-    const aboutDatas = [
-        {
-            "id": "1",
-            "title": "Fiabilité",
-            "content": "...",
-        },
-        {
-            "id": "2",
-            "title": "Respect",
-            "content": "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
-        },
-        {
-            "id": "3",
-            "title": "Service",
-            "content": "..."
-        },
-        {
-            "id": "4",
-            "title": "Sécurité",
-            "content": "..."
-        }
-    ]
+    const [Datas] = useState(
+        [
+            {
+                "id": "1",
+                "title": "Fiabilité",
+                "content": "...",
+            },
+            {
+                "id": "2",
+                "title": "Respect",
+                "content": "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+            },
+            {
+                "id": "3",
+                "title": "Service",
+                "content": "..."
+            },
+            {
+                "id": "4",
+                "title": "Sécurité",
+                "content": "..."
+            }
+        ]
+    );
 
     return (
         <div>
-            <Header />
-            <main className='a_propos'>
-                {aboutDatas.map(data => {
-                    return (
-                        <div key={data.id}>
-                        </div>
-                    )
-                }
-                )}
-            </main>
+        <Header />
+        <div className='a_propos_datas'>
+            
+            <table>
+                <tbody className='a_propos_datas_table'>
+                    {
+                        Datas.map(data => (
+                            <tr>
+                                <td>{data.title}</td>
+                                <td>{data.content}</td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
+        </div>
         </div>
     );
 };
